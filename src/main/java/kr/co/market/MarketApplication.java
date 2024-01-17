a1664106a3fb55d7b88a327c10c8c3aa5cc816de
jack <jack@heyratel.com>
2024-01-17 21:38:26 +0900

    테스트 커밋

--- src/main/java/kr/co/market/MarketApplication.java
+++ src/main/java/kr/co/market/MarketApplication.java
@@ -6,6 +6,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+//테스트 커밋
 public class MarketApplication {
 
     public static void main(String[] args) {
